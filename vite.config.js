ee46b1f86552a059489636297dcff742bfc8583f
Daniel Aguilar <daniel.aguilar7255@gmail.com>
2025-08-26 10:13:46 -0600

    Terminado?

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/landing_page/',
+  base: '/landing_page/',   
   plugins: [react()],
 })
